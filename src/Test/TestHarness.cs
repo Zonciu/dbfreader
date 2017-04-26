6257b73c42ddcb49cd9c2884ec63755e1f951306
mattschwartz <schwamat@gmail.com>
2017-04-26 10:00:59 -0500

    added proper test prj

---
+++ src/Test/TestHarness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class TestHarness
+    {
+        public TestHarness()
+        {
+        }
+
+        [Fact]
+        public void ShoulNotFail()
+        {
+            Assert.True(true);
+        }
+    }
+}
